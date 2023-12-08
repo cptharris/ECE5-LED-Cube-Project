a7e087ec896fc68dcee366fff1e8302b8f053394
Captain Harris <67209324+cptharris@users.noreply.github.com>
2023-12-07 17:45:22 -0800

    add another scene?

--- led-cube/led-cube.ino
+++ led-cube/led-cube.ino
@@ -1,6 +1,7 @@
 #include "CubeControl.h"
 
 CubeControl cube(12, 11, 8, 7, 6, 5, 4);
+const int wait = 100;
 
 void setup() {
   Serial.begin(115200);
@@ -13,7 +14,7 @@ void loop() {
       for (int k = 0; k < 4; k++) {
         // switch x, then y, then z
         cube.activate(k, j, i);
-        delay(100);
+        delay(wait);
       }
     }
   }
@@ -23,7 +24,7 @@ void loop() {
       for (int k = 0; k < 4; k++) {
         // switch y, then x, then z
         cube.activate(j, k, i);
-        delay(100);
+        delay(wait);
       }
     }
   }
@@ -33,7 +34,17 @@ void loop() {
       for (int k = 0; k < 4; k++) {
         // switch z, then y, then x
         cube.activate(i, j, k);
-        delay(100);
+        delay(wait);
+      }
+    }
+  }
+
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      for (int k = 0; k < 4; k++) {
+        // switch z, then x, then y
+        cube.activate(i, k, j);
+        delay(wait);
       }
     }
   }
