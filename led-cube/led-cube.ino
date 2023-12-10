3966f9d74e4c358d6c8dbc1493763f5ed5d26d15
andrewt-r <111332960+andrewt-r@users.noreply.github.com>
2023-12-09 20:57:51 -0800

    Update led-cube.ino
    added rain + tower 
    The tower has an extra feature (bug) where the last LED on the tower will be extra bright for some reason

--- led-cube/led-cube.ino
+++ led-cube/led-cube.ino
@@ -9,9 +9,10 @@ void setup() {
   randomSeed(analogRead(0));
 }
 
-void rain(int numDrops);
+void rain(int numDrops, unsigned long dropTime, unsigned long timeRain);
 void smile();
 void randomMadness();
+void Tower(int buildSpeed);
 
 void loop() {
   // smile();
@@ -19,9 +20,7 @@ void loop() {
 
   randomMadness();
 
-  rain((int)random(2, 7));
-  rain((int)random(2, 7));
-  rain((int)random(2, 7));
+  rain(8,100, 60000);
 
   for (int i = 0; i < 4; i++) {
     for (int j = 0; j < 4; j++) {
@@ -80,15 +79,65 @@ void smile() {
   cube.activateLayer(3);
 }
 
-void rain(int numDrops) {
-  int sum = 0;
-  int h = 3;
-  for (int i = 0; i < numDrops; i++) {
-    sum += cube.bin((int)random(0, 4), (int)random(0, 4));
+void rain(int numDrops, unsigned long dropTime, unsigned long timeRain) { 
+  int drops[numDrops][3];
+  unsigned long timeSinceDrop = 0;
+  unsigned long startTime = millis();
+  unsigned long timeSinceRainStart = millis();
+
+  for(int i = 0; i < numDrops; i++){
+    drops[i][0] = (int)random(0,3);
+    drops[i][1] = (int)random(0,3);
+    drops[i][2] = (int)random(0,3);
   }
-  while (h >= 0) {
-    cube.activateLayer(h--);
-    cube.write(sum);
-    delay(800);
+
+  while(startTime + timeRain >= millis()){
+    if(millis() - timeSinceDrop >= dropTime){
+      
+      timeSinceDrop = millis();
+      for(int i = 0; i < numDrops; i++){
+        drops[i][2]--;
+        if(drops[i][2] < 0){
+          drops[i][0] = (int)random(0,3);
+          drops[i][1] = (int)random(0,3);
+          drops[i][2] = 4;
+        }
+      } 
+    }
+
+    for(int i = 0; i < numDrops; i++){
+      cube.activate(drops[i][0], drops[i][1], drops[i][2]);
+    }
   }
+  return;
+}
+
+void Tower(int buildSpeed){
+  int layout[8][2] = {{0,1},{1,0},{2,0},{3,1},{3,2},{2,3},{1,3},{0,2}}; //Structure Im builder 
+  int onLEDs[32][3]; // onLEDs[i][0] = -1 for off LED
+  int LEDCounter = 0;
+  unsigned long timeSinceLed = millis();
+
+  for(int j = 0; j < 32; j++){ 
+    onLEDs[j][0] = -1; // -1 means don't display 
+  }
+
+  while(LEDCounter < 33){  
+    if(millis() - timeSinceLed >= buildSpeed){ //building the tower
+    
+      timeSinceLed = millis();
+      onLEDs[LEDCounter][0] = layout[LEDCounter%8][0];
+      onLEDs[LEDCounter][1] = layout[LEDCounter%8][1];
+      onLEDs[LEDCounter][2] = LEDCounter/8;
+      LEDCounter++;
+      
+    }
+
+    for(int i = 0; i < 32 ; i++){ //displaying the tower
+      if(onLEDs[i][0] != -1){
+        cube.activate(onLEDs[i][0], onLEDs[i][1], onLEDs[i][2]);   
+      } 
+    }
+  }
+  return;
 }
